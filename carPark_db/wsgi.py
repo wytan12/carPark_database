01129b4e2d990e860edf9ca1cdf88836339067bb
wytan12 <tanweiyin12@gmail.com>
2024-09-13 20:35:00 +0800

    update changes

--- carPark_db/wsgi.py
+++ carPark_db/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for carPark_db project.
+WSGI config for carPark_database project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carPark_db.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carPark_database.settings")
 
 application = get_wsgi_application()
